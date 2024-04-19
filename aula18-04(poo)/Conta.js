27fb75898e46c30d85f5330d292e8c247b96e262
Joel Santos <i3975@rn.senac.local>
2024-04-19 19:27:14 -0300

    Código Final Classe Conta

--- aula18-04(poo)/Conta.js
+++ aula18-04(poo)/Conta.js
@@ -1,16 +1,24 @@
 class Conta{
     titular;
     saldo;
-
     constructor(titular, saldo){
         this.titular = titular;
         this.saldo = saldo;
     }
     depositar(valor){
-        this.saldo+=valor;
+        //restrição valor maior que 0 e tipo de dado númerico
+        if(valor>0 && typeof valor == 'number'){
+           this.saldo+=valor; 
+        }else{
+           console.log('Valor inválido'); 
+        }
     }
     sacar(valor){
-        this.saldo-=valor;
+        if(valor>0 && typeof valor == 'number' && valor<=this.saldo){
+            this.saldo-=valor;
+        }else{
+            console.log('Valor inválido para o saque.');
+        }    
     }
     getSaldo(){
         return this.saldo;
--- aula18-04(poo)/main.js
+++ aula18-04(poo)/main.js
@@ -8,6 +8,8 @@ c1.sacar(50);
 console.table(c1);
 console.log(c1.getSaldo());
 c1.sacar(-5);
+c1.sacar(2000);
+c1.depositar('-9.5');
 console.table(c1);
 
 
