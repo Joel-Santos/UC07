11cf3e35e4c03bcb2818b973929ed57d875212dc
Joel Santos <i3975@rn.senac.local>
2024-05-07 18:25:27 -0300

    atualização

--- aula29-04/controllers/clienteController.js
+++ aula29-04/controllers/clienteController.js
@@ -5,7 +5,8 @@ let listaClientes = [];
 
 class ClienteCrontoller{
     home(req, res){
-        res.send('Rota dos clientes');
+        //res.send('Rota dos clientes');
+        res.sendFile(path.join(__dirname, '../public/html/cliente', 'homepageCliente.html'));
     }
     formCadastro(req, res){
         res.sendFile(path.join(__dirname, '../public/html/cliente', 'clienteForm.html'));
